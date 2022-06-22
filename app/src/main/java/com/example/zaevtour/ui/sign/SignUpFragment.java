12b7a19e9c194eb22316c583d34f6404c4b4012a
dajeongPark-dev <jul897813@gmail.com>
2022-06-22 12:11:47 +0900

    Text Gradient
    Text Gradient to a single TextView

--- app/src/main/java/com/example/zaevtour/ui/sign/SignUpFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/sign/SignUpFragment.java
@@ -2,6 +2,7 @@ package com.example.zaevtour.ui.sign;
 
 import androidx.lifecycle.ViewModelProvider;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,11 +12,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.captaindroid.tvg.Tvg;
 import com.example.zaevtour.R;
 
 public class SignUpFragment extends Fragment {
 
+    TextView greetingText;
+    EditText editUsernameEnter;
+    EditText editEmailEnter;
+    EditText editPWEnter;
+    EditText editPWCheck;
     private SignUpViewModel mViewModel;
 
     public static SignUpFragment newInstance() {
@@ -25,7 +34,19 @@ public class SignUpFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View v =  inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        greetingText = v.findViewById(R.id.greetingText);
+        editUsernameEnter = v.findViewById(R.id.editUsernameEnter);
+        editEmailEnter =v.findViewById(R.id.editEmailEnter);
+        editPWEnter = v.findViewById(R.id.editPWEnter);
+        editPWCheck =v.findViewById(R.id.editPWCheck);
+
+
+        // 텍스트 글자색 그라데이션 적용
+        Tvg.change(greetingText, Color.parseColor("#6C92F4"),  Color.parseColor("#41E884"));
+
+        return v;
     }
 
     @Override
