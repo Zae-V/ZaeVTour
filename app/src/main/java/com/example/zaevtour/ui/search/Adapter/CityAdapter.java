428c4ec2d0fc01ceda06348e0b3ad2fc7b4bb55d
Seyiul <63714074+Seyiul@users.noreply.github.com>
2022-06-24 21:55:00 +0900

    initial commit

---
+++ app/src/main/java/com/example/zaevtour/ui/search/Adapter/CityAdapter.java
@@ -0,0 +1,51 @@
+package com.example.zaevtour.ui.search.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.zaevtour.R;
+
+public class CityAdapter extends BaseAdapter {
+
+    Context context;
+    String[] cityName;
+
+    LayoutInflater inflater;
+
+    public CityAdapter(Context context, String[] cityName) {
+        this.context = context;
+        this.cityName = cityName;
+    }
+
+    @Override
+    public int getCount() {
+        return cityName.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        if(inflater == null)
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if(view == null){
+            view = inflater.inflate(R.layout.list_city,null);
+        }
+        TextView textView = view.findViewById(R.id.cityName);
+        textView.setText(cityName[i]);
+
+        return view;
+    }
+}
--- app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
@@ -4,14 +4,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.AdapterView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.zaevtour.MainActivity;
+import com.example.zaevtour.R;
 import com.example.zaevtour.databinding.FragmentProfileBinding;
 import com.example.zaevtour.databinding.FragmentSearchBinding;
+import com.example.zaevtour.ui.search.Adapter.CityAdapter;
+import com.example.zaevtour.ui.search.Adapter.VisitedAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 public class SearchFragment extends Fragment {
@@ -26,14 +32,34 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        final TextView textView = binding.textProfile;
-//        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        View v = inflater.inflate(R.layout.fragment_search, container, false);
+
+        String[] CityName = {"전체","강남구","강동구","강북구","강서구","관악구","광진구","구로구","금천구","노원구","도봉구","동대문구",
+        "동작구","마포구","서대문구","서초구","성동구","성북구","송파구","양천구","영등포구","용산구","은평구","종로구","종구","중랑구"};
+
+
+        String[] Visited = {"강남구","관악구","구로구"};
+        CityAdapter cityAdapter = new CityAdapter((getContext()),CityName);
+        binding.allList.setAdapter(cityAdapter);
+        binding.allList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(getContext(),CityName[i]+"을 선택하셨습니다.", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        VisitedAdapter recentCityAdapter = new VisitedAdapter(getContext(),Visited);
+        binding.recentVisit.setAdapter(recentCityAdapter);
+
         return root;
     }
+
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
     }
+
+
 }
