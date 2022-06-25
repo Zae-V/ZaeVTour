c42e4fdc9683c6000eee9714fcd73bd7334877fc
CHOSIYEON <tldusdk5@naver.com>
2022-06-25 21:29:06 +0900

    홈 화면 UI 완성

--- app/src/main/java/com/example/zaevtour/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.zaevtour.ui.home;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +11,8 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.captaindroid.tvg.Tvg;
+import com.example.zaevtour.R;
 import com.example.zaevtour.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
@@ -18,15 +21,15 @@ public class HomeFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        HomeViewModel homeViewModel =
-                new ViewModelProvider(this).get(HomeViewModel.class);
+//        HomeViewModel homeViewModel =
+//                new ViewModelProvider(this).get(HomeViewModel.class);
 
-        binding = FragmentHomeBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        // 텍스트 Gradient 적용
+        View v = inflater.inflate(R.layout.fragment_home, container, false);
+        TextView textView = v.findViewById(R.id.titleText);
+        Tvg.change(textView, Color.parseColor("#8CEDB3"), Color.parseColor("#6C92F4"));
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        return v;
     }
 
     @Override
