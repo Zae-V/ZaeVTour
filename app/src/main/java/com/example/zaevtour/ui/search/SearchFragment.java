d7ead052401b4a5ab699b1e601a4c921af568cc8
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-06-29 00:53:04 +0900

    searchbar, 버튼 수정

--- app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
@@ -7,9 +7,11 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.zaevtour.R;
 import com.example.zaevtour.databinding.FragmentProfileBinding;
 import com.example.zaevtour.databinding.FragmentSearchBinding;
 
@@ -17,7 +19,6 @@ import com.example.zaevtour.databinding.FragmentSearchBinding;
 public class SearchFragment extends Fragment {
 
     private FragmentSearchBinding binding;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         SearchViewModel searchViewModel =
@@ -26,6 +27,14 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+//        SearchView searchBar = (SearchView)getView().findViewById(R.id.searchBar);
+        SearchView searchBar = root.findViewById(R.id.searchBar);
+        searchBar.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                searchBar.setIconified(false);
+            }
+        });
 //        final TextView textView = binding.textProfile;
 //        searchViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
