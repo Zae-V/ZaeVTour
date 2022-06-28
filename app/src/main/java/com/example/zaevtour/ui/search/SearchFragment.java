bbfa760b45a8ab5cc2b9dfd21d30c3e8995ab7ae
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-06-29 01:14:38 +0900

    searchfragment 연결:blush:

--- app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
@@ -1,16 +1,21 @@
 package com.example.zaevtour.ui.search;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.zaevtour.IntroActivity;
+import com.example.zaevtour.MainActivity;
 import com.example.zaevtour.R;
 import com.example.zaevtour.databinding.FragmentProfileBinding;
 import com.example.zaevtour.databinding.FragmentSearchBinding;
@@ -28,7 +33,19 @@ public class SearchFragment extends Fragment {
         binding = FragmentSearchBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-//        SearchView searchBar = (SearchView)getView().findViewById(R.id.searchBar);
+        Button spinnerBtn = root.findViewById(R.id.spinnerBtn);
+        spinnerBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Fragment searchFragment2 = new SearchFragment2();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.searchContainer, searchFragment2);
+                transaction.addToBackStack(null);
+
+                transaction.commit();
+            }
+        });
+
         SearchView searchBar = root.findViewById(R.id.searchBar);
         searchBar.setOnClickListener(new View.OnClickListener(){
             @Override
