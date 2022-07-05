620d2193fb27234758352732bb1b3b5223f04e54
SeonDahye <81613151+SeonDahye@users.noreply.github.com>
2022-07-05 23:11:20 +0900

    API 등록

--- app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/search/SearchFragment.java
@@ -6,18 +6,17 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.zaevtour.IntroActivity;
 import com.example.zaevtour.MainActivity;
+import com.example.zaevtour.PloggingActivity;
 import com.example.zaevtour.R;
-import com.example.zaevtour.databinding.FragmentProfileBinding;
+import com.example.zaevtour.PlaceActivity;
+import com.example.zaevtour.VeganActivity;
 import com.example.zaevtour.databinding.FragmentSearchBinding;
 
 
@@ -48,6 +47,33 @@ public class SearchFragment extends Fragment {
             @Override
             public void onClick(View v){
                 searchBar.setIconified(false);
+            }
+        });
+
+        Button spotBtn = root.findViewById(R.id.spotBtn);
+        spotBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PlaceActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button ploggingBtn = root.findViewById(R.id.ploggingBtn);
+        ploggingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), PloggingActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button restaurantBtn = root.findViewById(R.id.restaurantBtn);
+        restaurantBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), VeganActivity.class);
+                startActivity(intent);
             }
         });
 //        final TextView textView = binding.textProfile;
