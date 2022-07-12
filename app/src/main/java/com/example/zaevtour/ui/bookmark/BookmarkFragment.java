e7effc5a10353d3683ce19064852d24ac70c9725
dajeongPark-dev <jul897813@gmail.com>
2022-07-12 10:39:21 +0900

    Update fragment_bookmark.xml

--- app/src/main/java/com/example/zaevtour/ui/bookmark/BookmarkFragment.java
+++ app/src/main/java/com/example/zaevtour/ui/bookmark/BookmarkFragment.java
@@ -60,12 +60,22 @@ public class BookmarkFragment extends Fragment {
         BookmarkItem bookmarkItem3 = new BookmarkItem(R.drawable.vegan_burger,"비건버거","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
         BookmarkItem bookmarkItem4 = new BookmarkItem(R.drawable.vegan_burger,"초록숲상점","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
         BookmarkItem bookmarkItem5 = new BookmarkItem(R.drawable.vegan_burger,"제로웨이스트샵","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
+        BookmarkItem bookmarkItem6 = new BookmarkItem(R.drawable.vegan_burger,"제비식당","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
+        BookmarkItem bookmarkItem7 = new BookmarkItem(R.drawable.vegan_burger,"야채꼬치","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
+        BookmarkItem bookmarkItem8 = new BookmarkItem(R.drawable.vegan_burger,"비건버거","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
+        BookmarkItem bookmarkItem9 = new BookmarkItem(R.drawable.vegan_burger,"초록숲상점","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
+        BookmarkItem bookmarkItem10 = new BookmarkItem(R.drawable.vegan_burger,"제로웨이스트샵","서울시 강서구 개화동", "AM 9:00 ~ PM 10:00");
 
         listAdapter.addItem(bookmarkItem1);
         listAdapter.addItem(bookmarkItem2);
         listAdapter.addItem(bookmarkItem3);
         listAdapter.addItem(bookmarkItem4);
         listAdapter.addItem(bookmarkItem5);
+        listAdapter.addItem(bookmarkItem6);
+        listAdapter.addItem(bookmarkItem7);
+        listAdapter.addItem(bookmarkItem8);
+        listAdapter.addItem(bookmarkItem9);
+        listAdapter.addItem(bookmarkItem10);
 
         return v;
     }
